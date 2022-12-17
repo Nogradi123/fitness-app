2372d78dae29eeb20f35850ee720bdedea705a1d
Nogradi123 <nogradi.nicholas1@gmail.com>
2022-12-17 15:54:39 -0500

    entered API key

--- src/fetchData.js
+++ src/fetchData.js
@@ -1,7 +1,7 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': '7551bd998fmshcd2c2220e76f8e9p1add01jsne453ce90f67f',
+        'X-RapidAPI-Key': process.env.APIKEY,
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
   };
